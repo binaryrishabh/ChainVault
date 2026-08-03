d1660797e27ce41fc833f3efa81c76a38f2490a3
BinaryRishabh <srivastavarishabh2001@gmail.com>
2026-08-03 23:57:44 +0530

    Phase one of backend i.e. till sliding window log rate limiter done

--- Backend/utils/rateLimiter.ts
+++ Backend/utils/rateLimiter.ts
@@ -1,17 +1,68 @@
 import type { Request, Response, NextFunction } from "express";
 
-const MAX_REQUESTS = 100;
-const WINDOW_MAX_SIZE = 15 * 60 * 1000; // 15 minutes
+const MAX_REQUESTS = 10;
+const TIME_WINDOW_MAX_SIZE = 15000; // 15 minutes
 
-const ipMap = new Map<string, { count: number; resetTime: number }>();
+// Rate limiting algorithm: Sliding Window Log
+const ipMap = new Map<string, number[]>();
+
+// Clean up ip's who never visited within TIME_WINDOW_MAX_SIZE minutes after there last interaction 
+// which helps to keep the map with interacting ips only.
+setInterval(() => {
+    for(const [key, value] of ipMap) {
+        if(value.length === 0 || Date.now() - value[value.length - 1]! > TIME_WINDOW_MAX_SIZE) {
+            ipMap.delete(key);
+        }
+    }
+}, TIME_WINDOW_MAX_SIZE);
+
+export const rateLimiter = (req: Request, res: Response, next: NextFunction) => {
+    const ip = req.ip || req.socket.remoteAddress || "unknown" ;
+    const now = Date.now();
+    let timeStamp = ipMap.get(ip) || [];
+
+    timeStamp = timeStamp.filter(timeStampLog => now - timeStampLog < TIME_WINDOW_MAX_SIZE);
+
+    if(timeStamp.length < MAX_REQUESTS) {
+        timeStamp = [...timeStamp, now];
+        ipMap.set(ip, timeStamp);
+        res.setHeader("X-RateLimit-Limit", MAX_REQUESTS);
+        res.setHeader("X-RateLimit-Remaining", MAX_REQUESTS - timeStamp.length);
+        res.setHeader("X-RateLimit-Reset", timeStamp[0]! + TIME_WINDOW_MAX_SIZE);
+        next();
+        return;
+    }
+    else {
+        res.setHeader("Retry-After", Math.ceil((timeStamp[0]! + TIME_WINDOW_MAX_SIZE - now) / 1000)) // in seconds
+        res.setHeader("X-RateLimit-Limit", MAX_REQUESTS);
+        res.setHeader("X-RateLimit-Remaining", 0);
+        res.setHeader("X-RateLimit-Reset", timeStamp[0]! + TIME_WINDOW_MAX_SIZE);
+        res.status(429).json({
+            success: false,
+            message: "Too many requests"
+        })
+        return;
+    }
+}
+
 
 // Rate limiting algorithm: Fixed Window Counter
+/* 
+const ipMap = new Map<string, { count: number; resetTime: number }>();
+
+// Clean up ip's who never visited within 15 minutes after there interaction which helps to keep the map size with interacting ips only.
+setInterval(() => {
+    for(const [key, value] of ipMap) {
+        if(value.resetTime < Date.now()) {
+            ipMap.delete(key);
+        }
+    }
+}, TIME_WINDOW_MAX_SIZE)
+
 // Per IP: 100 requests per 15-minute window
 // Resets when window expires
 export const rateLimiter = (req: Request, res: Response, next: NextFunction) => {
     const ip = req.ip || req.socket.remoteAddress || "unknown";
-    console.log(ip);
-    
 
     const now = Date.now();
 
@@ -21,12 +72,12 @@ export const rateLimiter = (req: Request, res: Response, next: NextFunction) => 
         next();
         return;
     }
-    
+
     if(!entry || entry.resetTime < now) { // ip not exists or window time has past so reset
-        ipMap.set(ip, { count: 1, resetTime: now + WINDOW_MAX_SIZE })
+        ipMap.set(ip, { count: 1, resetTime: now + TIME_WINDOW_MAX_SIZE })
         res.setHeader("X-RateLimit-Limit", MAX_REQUESTS);
         res.setHeader("X-RateLimit-Remaining", MAX_REQUESTS - 1);
-        res.setHeader("X-RateLimit-Reset", now + WINDOW_MAX_SIZE);
+        res.setHeader("X-RateLimit-Reset", now + TIME_WINDOW_MAX_SIZE);
         next();
         return;
     }
@@ -41,7 +92,7 @@ export const rateLimiter = (req: Request, res: Response, next: NextFunction) => 
     else { // if ip exists && reset time >= now &&  count > MAX_REQUESTS: reject as already filled
         res.setHeader("X-RateLimit-Limit", MAX_REQUESTS);
         res.setHeader("X-RateLimit-Remaining", 0);
-        res.setHeader("Retry-After", Math.ceil((entry.resetTime - now) / 1000)) // in minutes
+        res.setHeader("Retry-After", Math.ceil((entry.resetTime - now) / 1000)) // in seconds
         res.setHeader("X-RateLimit-Reset", entry.resetTime);
         res.status(429).json({
             success: false,
@@ -50,3 +101,4 @@ export const rateLimiter = (req: Request, res: Response, next: NextFunction) => 
         return;
     }
 }
+ */
