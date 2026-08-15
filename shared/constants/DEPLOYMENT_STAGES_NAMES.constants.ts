1778a2f2a29a691678f1255d6f5f3d0297aa196f
BinaryRishabh <srivastavarishabh2001@gmail.com>
2026-08-15 11:31:44 +0530

    Corrected a spelling

--- shared/constants/DEPLOYMENT_STAGES_NAMES.constants.ts
+++ shared/constants/DEPLOYMENT_STAGES_NAMES.constants.ts
@@ -8,6 +8,6 @@ export const DEPLOYMENT_STAGES_NAMES = [
   "CostEstimate",
   "MonitorSetup",
   "Ready"
-] as const; // This tells to typescript that this array is readOnly and vakue are exact literals not just string[]. Without as const, the type is string[]. With it, the type is readonly ["Validate", "Provision", "Configure"].
+] as const; // This tells to typescript that this array is readOnly and value are exact literals not just string[]. Without as const, the type is string[]. With it, the type is readonly ["Validate", "Provision", "Configure"].
 
 export type DeploymentStagesNamesType = (typeof DEPLOYMENT_STAGES_NAMES)[number];
