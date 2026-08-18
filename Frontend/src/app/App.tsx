e783846a4fc4d0b3302708a88a3455cbfe1ba2ef
BinaryRishabh <srivastavarishabh2001@gmail.com>
2026-08-19 00:34:24 +0530

    Moved App.tsx file here

---
+++ Frontend/src/app/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CanvasDesignerPage } from "../features/canvas/components/CanvasDesignerPage";
+import { MonitoringDashboard } from "../MonitoringView/MonitoringDashboard";
+import { Toaster } from "sonner";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Toaster 
+        position="top-right"
+        offset={64}
+        theme="dark"
+        toastOptions={{
+          style: {
+            background: "#12161F",
+            border: "1px solid #273042",
+            color: "#EDF1F7"
+          }
+        }}
+      />
+      <Routes>
+        <Route path="/" element={<CanvasDesignerPage />} />
+        <Route path="/deployments/:deploymentId" element={ <MonitoringDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
